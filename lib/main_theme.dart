04fa61ef8839ed558e41cdec87f7215e97b5254a
EAhumada08 <emaahmc08@gmail.com>
2025-03-22 21:06:03 -0600

    implementar tema obscuro

--- lib/main_theme.dart
+++ lib/main_theme.dart
@@ -3,15 +3,10 @@ import 'package:gasto_0/core/const.dart';
 
 ThemeData mainTheme = ThemeData(
   primaryColor: primaryColor,
-  primaryTextTheme: TextTheme(
-    labelMedium: TextStyle(
-        fontSize: 24.0, color: primaryColor, fontWeight: FontWeight.bold),
-    titleLarge: TextStyle(fontSize: 36.0, color: primaryColor),
-    titleMedium: TextStyle(fontSize: 28.0, color: Colors.black),
-  ),
+  brightness: Brightness.dark,
   colorScheme: ColorScheme.fromSeed(
+    brightness: Brightness.dark,
     seedColor: primaryColor,
-    primary: primaryColor,
   ),
   useMaterial3: true,
 );
