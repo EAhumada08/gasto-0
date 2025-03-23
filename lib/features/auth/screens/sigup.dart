9f7f0e4659169f2bf80022bddf6eae715ac13c5f
EAhumada08 <emaahmc08@gmail.com>
2025-03-22 21:07:32 -0600

    Creacion de pantalla SignUp

---
+++ lib/features/auth/screens/sigup.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:gasto_0/features/auth/widgets/input_login.dart';
+
+class SignUp extends StatelessWidget {
+  SignUp({super.key});
+  final TextEditingController nombreController = TextEditingController();
+  final TextEditingController apellidoController = TextEditingController();
+  final TextEditingController correoController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 50,
+          children: [
+            Text(
+              'Registrate',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 10,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  spacing: 10,
+                  children: [
+                    Inputlogin(label: 'Nombre', controller: nombreController),
+                    Inputlogin(
+                        label: 'Apellidos', controller: apellidoController),
+                  ],
+                ),
+                Inputlogin(label: 'Correo', controller: correoController),
+                Inputlogin(
+                  label: 'Contrasena',
+                  controller: passwordController,
+                  isPassword: true,
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text('Ya tienes cuenta? '),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('Inicia sesión'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
